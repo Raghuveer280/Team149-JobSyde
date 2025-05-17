24a904d6464c19cd83687f4b3d734dedbcfa8833
Raghuveer280 <raghu59rk@gmail.com>
2025-05-17 18:22:22 +0530

    home and login page added

---
+++ src/main/java/com/example/demo/Team149JobSydeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team149JobSydeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team149JobSydeApplication.class, args);
+	}
+
+}
