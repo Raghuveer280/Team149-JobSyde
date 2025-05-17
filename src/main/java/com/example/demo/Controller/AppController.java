022b9ba45d63cd4d95ffa0f7a382e5bec2d0c7e6
Raghuveer280 <raghu59rk@gmail.com>
2025-05-17 19:55:27 +0530

    Login backend added

---
+++ src/main/java/com/example/demo/Controller/AppController.java
@@ -0,0 +1,49 @@
+package com.example.demo.Controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpSession;
+
+
+@Controller
+public class AppController {
+	@GetMapping("/")
+	public String index(HttpSession session,Model m)
+	{
+		String phn=(String)session.getAttribute("username");
+		if(phn!=null)
+		{
+			m.addAttribute("logged", "login");
+		}
+		return "index";
+	}
+	@GetMapping("/login")
+	public String login()
+	{
+		return "login";
+	}
+	@GetMapping("/serviceLogin")
+	public String serviceLogin()
+	{
+		return "serviceLogin";
+	}
+	@GetMapping("/createprofile")
+	public String createprofile()
+	{
+		return "createprofile";
+	}
+	@GetMapping("/customerRegistration")
+	public String customerRegistration()
+	{
+		return "customerRegistration";
+	}
+	@GetMapping("/logout")
+	public void logout(HttpSession session)
+	{
+		session.invalidate();
+	}
+	
+}
